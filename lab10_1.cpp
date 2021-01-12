6d9001af379f29cd2590ddc58b981f886c380d87
BottlesMan <man2545_@hotmail.com>
2021-01-12 18:48:03 +0700

    Done

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+string Grade(int x){
+	if(x == 0) return "A";
+	else if(x == 1) return "B+";
+	else if(x == 2) return "B";
+	else if(x == 3) return "C+";
+	else if(x == 4) return "C";
+	else if(x == 5) return "D+";
+	else if(x == 6) return "D";
+	else if(x == 7) return "F";
+	else return "W";
+}
+
+int main(){
+	srand(time(0));
+	cout << "Press Enter 3 times to reveal your future.";
+	for(int i = 0; i < 3; i++){
+		cin.get();
+	}
+	int x = rand()%9;
+	cout << "You will get " << Grade(x) << " in this 261102.";
+	return 0;
+}
